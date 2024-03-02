da03a5628a418a191fd88b4642c66597c8e53f4e
vuisme <me@vutn.net>
2024-03-02 09:44:07 +0700

    test

--- src/components/player/index.tsx
+++ src/components/player/index.tsx
@@ -1,1 +1,19 @@
 export * as Player from "./Player";
+import { forwardRef } from "react";
+
+interface MediaGridProps {
+  children?: React.ReactNode;
+}
+
+export const MediaGrid = forwardRef<HTMLDivElement, MediaGridProps>(
+  (props, ref) => {
+    return (
+      <div
+        className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4"
+        ref={ref}
+      >
+        {props.children}
+      </div>
+    );
+  },
+);
